ec787067001371a5335676eb60b3fa7ac6a28612
Benjamin White <benjamin.e.white1@gmail.com>
2016-11-08 09:16:07 -0500

    in progress

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express();
 
 const shoppingList = require('./shoppingList');
 
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
 app.use('/items', shoppingList);
 
 app.listen(process.env.PORT || 8080, () => {
