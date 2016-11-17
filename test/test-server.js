ff9bf260356f948c2e4a62008bbe570b3c037642
Benjamin White <benjamin.e.white1@gmail.com>
2016-11-17 16:34:31 -0500

    initial test stubs

---
+++ test/test-server.js
@@ -0,0 +1,6 @@
+describe('Shopping List', function() {
+  it('should list items on GET');
+  it('should add an item on POST');
+  it('should edit an item on PUT');
+  it('should delete an item on DELETE');
+});
