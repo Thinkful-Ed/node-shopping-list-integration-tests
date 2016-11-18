a35cbbd03da4f24467c56facdb597aac15393e16
Benjamin White <benjamin.e.white1@gmail.com>
2016-11-17 19:29:57 -0500

    test for 200 on get items passing

--- test/test-server.js
+++ test/test-server.js
@@ -3,10 +3,19 @@ const chaiHttp = require('chai-http');
 const app = require('../server.js');
 const {ShoppingList} = require('../models');
 
+const should = chai.should();
+
 chai.use(chaiHttp);
 
 describe('Shopping List', function() {
-  it('should list items on GET');
+  it('should list items on GET', function(done) {
+    chai.request(app)
+      .get('/shopping-list')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        done();
+      })
+  });
   it('should add an item on POST');
   it('should edit an item on PUT');
   it('should delete an item on DELETE');
